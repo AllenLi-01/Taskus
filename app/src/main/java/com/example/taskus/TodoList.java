67d54bace5abbb6f6c90f19cca848433c3e7fb0a
Allen Li <lishuxin2001@qq.com>
2023-04-25 17:01:11 +0800

    update copyright information

--- app/src/main/java/com/example/taskus/TodoList.java
+++ app/src/main/java/com/example/taskus/TodoList.java
@@ -1,3 +1,14 @@
+/*
+ * Copyright (c) 2023 Allen Li
+ *
+ * All rights reserved.
+ *
+ * This software is the property of Allen Li and is protected by copyright,
+ * trademark, and other intellectual property laws. You may not reproduce, modify,
+ * distribute, or create derivative works based on this software, in whole or in part,
+ * without the express written permission of Allen Li.
+ */
+
 package com.example.taskus;
 
 import java.util.List;
